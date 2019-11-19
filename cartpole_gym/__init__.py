a7810e248070fbde193b37c6911f411a4e123f3f
rebvivi <chengvivian1225@gmail.com>
2019-11-19 15:32:23 +0800

    add q_learning cartpole

--- cartpole_gym/__init__.py
+++ cartpole_gym/__init__.py
@@ -3,7 +3,7 @@ from gym.envs.registration import register
 register(
     id='CartPoleEnv11-v0',
     entry_point='cartpole_gym.envs:CartPoleEnv',
-    max_episode_steps=200,
+    max_episode_steps=2000,
     reward_threshold=195.0,
 )
 
